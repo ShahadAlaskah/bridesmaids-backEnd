04e4ecfa42d793985199cc5c4c5da62dc28773b9
unknown <leenahalkhuraiji@gmail.com>
2022-09-18 10:13:34 -0700

    fix user approve endpoint

--- src/main/java/com/example/bridesmaids/repository/UserRepository.java
+++ src/main/java/com/example/bridesmaids/repository/UserRepository.java
@@ -4,11 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findUserById(Integer id);
     User findUserByUsername(String username);
     @Query("select u from User u where u.isApproved=false")
-    User findUserByIsApproved();
+    List<User> findAllByIsApproved();
 
 }
